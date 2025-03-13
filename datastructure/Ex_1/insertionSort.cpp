67a3397f4f667f5c589b2feb04586ecac4c86284
KGB <kkggbbpoint923@gmail.com>
2025-03-14 00:09:42 +0900

    study insertion_sorting

--- datastructure/Ex_1/insertionSort.cpp
+++ datastructure/Ex_1/insertionSort.cpp
@@ -1,5 +1,32 @@
 #include <stdio.h>
-
+void insertionSort(int arr[], int n) { //n is lenght of array
+	int i, j, key; 
+	for (i = 1; i < n; i++) {
+		key = arr[i];
+		j = i - 1;
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
 int main() {
-	printf("Hello World\n");
+	int arr[] = {10, 1, 5, 7, 8, 4, 6, 2, 9, 3};
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	printf("Before Sorting\n"); //print original array
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n\n");
+
+	insertionSort(arr, n); //insertion sorting
+
+	printf("After Sorting\n"); //print sorted array
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n\n");
+	return 0;
 }
